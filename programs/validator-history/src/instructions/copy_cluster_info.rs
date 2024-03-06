eb131bb33d8a63cd43b90e827473ef85a22b4e50
Evan B <ebatsell@gmail.com>
2024-03-06 18:19:06 -0500

    Fix compute budget overflow on SlotHistory reading (#25)

--- programs/validator-history/src/instructions/copy_cluster_info.rs
+++ programs/validator-history/src/instructions/copy_cluster_info.rs
@@ -6,7 +6,10 @@ use {
     },
     anchor_lang::{
         prelude::*,
-        solana_program::{clock::Clock, slot_history::Check},
+        solana_program::{
+            clock::Clock,
+            slot_history::{Check, MAX_ENTRIES},
+        },
     },
 };
 
@@ -36,12 +39,30 @@ pub fn handle_copy_cluster_info(ctx: Context<CopyClusterInfo>) -> Result<()> {
 
     let epoch_start_timestamp = cast_epoch_start_timestamp(clock.epoch_start_timestamp);
 
-    // Sets the slot history for the previous epoch, since the current epoch is not yet complete.
-    if epoch > 0 {
-        cluster_history_account
-            .set_blocks(epoch - 1, blocks_in_epoch(epoch - 1, &slot_history)?)?;
-    }
-    cluster_history_account.set_blocks(epoch, blocks_in_epoch(epoch, &slot_history)?)?;
+    let epoch_schedule = EpochSchedule::get()?;
+
+    let slot_history = if epoch > 0 {
+        let slot_history_next_slot = slot_history.next_slot;
+        let start_slot = epoch_schedule.get_first_slot_in_epoch((epoch - 1).into());
+        let end_slot = epoch_schedule.get_last_slot_in_epoch((epoch - 1).into());
+        let (num_blocks, bitvec_inner) =
+            confirmed_blocks_in_epoch(start_slot, end_slot, *slot_history)?;
+        // Sets the slot history for the previous epoch, since the total number of blocks in the epoch is now final
+        cluster_history_account.set_blocks(epoch - 1, num_blocks)?;
+        // The original bits are consumed by the set_blocks method, so this recreates
+        // SlotHistory with same heap memory chunk, with no modifications
+        Box::new(SlotHistory {
+            bits: bitvec_inner.into(),
+            next_slot: slot_history_next_slot,
+        })
+    } else {
+        slot_history
+    };
+
+    let start_slot = epoch_schedule.get_first_slot_in_epoch(epoch.into());
+    let end_slot = epoch_schedule.get_last_slot_in_epoch(epoch.into());
+    let (num_blocks, _) = confirmed_blocks_in_epoch(start_slot, end_slot, *slot_history)?;
+    cluster_history_account.set_blocks(epoch, num_blocks)?;
     cluster_history_account.set_epoch_start_timestamp(epoch, epoch_start_timestamp)?;
 
     cluster_history_account.cluster_history_last_update_slot = clock.slot;
@@ -49,13 +70,39 @@ pub fn handle_copy_cluster_info(ctx: Context<CopyClusterInfo>) -> Result<()> {
     Ok(())
 }
 
-fn blocks_in_epoch(epoch: u16, slot_history: &SlotHistory) -> Result<u32> {
-    let epoch_schedule = EpochSchedule::get()?;
-    let start_slot = epoch_schedule.get_first_slot_in_epoch(epoch as u64);
-    let end_slot = epoch_schedule.get_last_slot_in_epoch(epoch as u64);
+const BITVEC_BLOCK_SIZE: u64 = 64;
 
-    let mut blocks_in_epoch = 0;
-    for i in start_slot..=end_slot {
+pub fn confirmed_blocks_in_epoch(
+    start_slot: u64,
+    end_slot: u64,
+    slot_history: SlotHistory,
+) -> Result<(u32, Box<[u64]>)> {
+    // The SlotHistory BitVec wraps a slice of "Blocks", usizes representing 64 slots each (different than solana blocks).
+    // It contains the last 1,048,576 slots, which means it always has all slots from the previous and current epoch.
+    // Iterating through each slot uses too much compute, but we can count the bits of each u64 altogether efficiently
+    // with `.count_ones()`.
+    // The epoch is not guaranteed to align perfectly with Blocks so we need to count the first and last partial Blocks separately.
+    // The bitvec inner data is taken ownership of, then returned to be reused.
+    let mut blocks_in_epoch: u32 = 0;
+
+    let first_full_block_slot = if start_slot % BITVEC_BLOCK_SIZE == 0 {
+        start_slot
+    } else {
+        start_slot
+            .checked_add(
+                BITVEC_BLOCK_SIZE
+                    .checked_sub(start_slot % BITVEC_BLOCK_SIZE)
+                    .ok_or(ValidatorHistoryError::ArithmeticError)?,
+            )
+            .ok_or(ValidatorHistoryError::ArithmeticError)?
+    };
+
+    let last_full_block_slot = end_slot
+        .checked_sub(end_slot % BITVEC_BLOCK_SIZE)
+        .ok_or(ValidatorHistoryError::ArithmeticError)?;
+
+    // First and last slots, in partial blocks
+    for i in (start_slot..first_full_block_slot).chain(last_full_block_slot..=end_slot) {
         match slot_history.check(i) {
             Check::Found => {
                 blocks_in_epoch += 1;
@@ -72,5 +119,14 @@ fn blocks_in_epoch(epoch: u16, slot_history: &SlotHistory) -> Result<u32> {
         };
     }
 
-    Ok(blocks_in_epoch)
+    let inner_bitvec = slot_history.bits.into_boxed_slice();
+
+    for i in (first_full_block_slot..last_full_block_slot).step_by(BITVEC_BLOCK_SIZE as usize) {
+        let block_index = (i % MAX_ENTRIES) / BITVEC_BLOCK_SIZE;
+        blocks_in_epoch = blocks_in_epoch
+            .checked_add(inner_bitvec[block_index as usize].count_ones())
+            .ok_or(ValidatorHistoryError::ArithmeticError)?;
+    }
+
+    Ok((blocks_in_epoch, inner_bitvec))
 }
