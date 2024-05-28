a23991c27b9979e05f53118254eb68c4c81188e1
Coach Chuck <christian@jito.wtf>
2024-05-28 11:41:41 -0600

    added testing script

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run all tests except the specified one
+SBF_OUT_DIR=$(pwd)/target/deploy RUST_MIN_STACK=5000000 cargo test -- --skip steward::test_state_methods
+
+# Check if the previous command succeeded
+if [ $? -eq 0 ]; then
+    # Run the specific test in isolation
+    SBF_OUT_DIR=$(pwd)/target/deploy RUST_MIN_STACK=5000000 cargo test --package tests --test mod steward::test_state_methods
+else
+    echo "Some tests failed, skipping the isolated test run."
+fi
