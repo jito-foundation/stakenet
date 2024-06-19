6af54e082c84382a5c34868a4acb782e8e08f444
Christian <christian@jito.wtf>
2024-06-19 13:35:44 -0600

    closter

--- keepers/keeper-core/src/lib.rs
+++ keepers/keeper-core/src/lib.rs
@@ -30,6 +30,14 @@ pub struct SubmitStats {
     pub errors: u64,
     pub results: Vec<Result<(), SendTransactionError>>,
 }
+
+impl SubmitStats {
+    pub fn combine(&mut self, other: &SubmitStats) {
+        self.successes += other.successes;
+        self.errors += other.errors;
+        self.results.extend(other.results.clone())
+    }
+}
 #[derive(Debug, Default, Clone)]
 pub struct CreateUpdateStats {
     pub creates: SubmitStats,
