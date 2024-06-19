4b84286117eb36ecb41ff0dae2c2b291a3787623
Christian <christian@jito.wtf>
2024-06-19 13:35:28 -0600

    closter

--- utils/steward-cli/src/commands/monkey/crank.rs
+++ utils/steward-cli/src/commands/monkey/crank.rs
@@ -1,10 +1,11 @@
 use std::sync::Arc;
 
 use anchor_lang::{InstructionData, ToAccountMetas};
-use anyhow::{Ok, Result};
 use jito_steward::StewardStateEnum;
-use solana_client::nonblocking::rpc_client::RpcClient;
+use keeper_core::{MultipleAccountsError, SubmitStats, TransactionExecutionError};
+use solana_client::{client_error::ClientError, nonblocking::rpc_client::RpcClient};
 use solana_program::instruction::Instruction;
+use thiserror::Error as ThisError;
 
 use solana_sdk::{
     pubkey::Pubkey,
@@ -17,21 +18,33 @@ use crate::{
     commands::command_args::CrankMonkey,
     utils::{
         accounts::{
-            get_all_steward_accounts, get_steward_state_account, get_steward_state_address,
-            UsefulStewardAccounts,
+            get_all_steward_accounts, get_cluster_history_address, get_steward_state_account,
+            get_validator_history_address, UsefulStewardAccounts,
         },
-        transactions::configure_instruction,
+        transactions::{configure_instruction, package_instructions, submit_packaged_transactions},
     },
 };
+
+#[derive(ThisError, Debug)]
+pub enum MonkeyCrankError {
+    #[error(transparent)]
+    ClientError(#[from] ClientError),
+    #[error(transparent)]
+    TransactionExecutionError(#[from] TransactionExecutionError),
+    #[error(transparent)]
+    MultipleAccountsError(#[from] MultipleAccountsError),
+    #[error("Custom: {0}")]
+    Custom(String),
+}
 
 async fn _handle_epoch_maintenance(
     payer: &Arc<Keypair>,
     client: &Arc<RpcClient>,
     program_id: &Pubkey,
+    epoch: u64,
     all_steward_accounts: &Box<UsefulStewardAccounts>,
     priority_fee: Option<u64>,
-) -> Result<()> {
-    let mut current_epoch = client.get_epoch_info().await?.epoch;
+) -> Result<SubmitStats, MonkeyCrankError> {
     let mut state_epoch = all_steward_accounts.state_account.state.current_epoch;
     let mut num_validators = all_steward_accounts.state_account.state.num_pool_validators;
     let mut validators_to_remove = all_steward_accounts
@@ -39,10 +52,19 @@ async fn _handle_epoch_maintenance(
         .state
         .validators_to_remove;
 
-    while state_epoch != current_epoch {
+    let mut stats = SubmitStats::default();
+
+    while state_epoch != epoch {
         let mut validator_index_to_remove = None;
         for i in 0..num_validators {
-            if validators_to_remove.get(i)? {
+            if validators_to_remove.get(i).map_err(|e| {
+                MonkeyCrankError::Custom(format!(
+                    "Error fetching bitmask index for removed validator: {}/{} - {}",
+                    i,
+                    num_validators,
+                    e.to_string()
+                ))
+            })? {
                 validator_index_to_remove = Some(i);
                 break;
             }
@@ -63,36 +85,26 @@ async fn _handle_epoch_maintenance(
             .data(),
         };
 
-        let blockhash = client.get_latest_blockhash().await?;
         let configured_ix = configure_instruction(&[ix], priority_fee, None, None);
 
-        let transaction = Transaction::new_signed_with_payer(
-            &configured_ix,
-            Some(&payer.pubkey()),
-            &[&payer],
-            blockhash,
-        );
+        let new_stats =
+            submit_packaged_transactions(client, vec![configured_ix], &payer, None, None).await?;
 
-        let signature = client
-            .send_and_confirm_transaction_with_spinner(&transaction)
-            .await?;
+        stats.combine(&new_stats);
 
-        println!(
-            "EPOCH MAINTENANCE: Removed validator index {:?} : {:?}",
-            validator_index_to_remove, signature
-        );
-
+        // NOTE: This is the only time an account is fetched
+        // in any of these cranking functions
         let updated_state_account =
             get_steward_state_account(client, &program_id, &all_steward_accounts.config_address)
-                .await?;
+                .await
+                .unwrap();
 
         num_validators = updated_state_account.state.num_pool_validators;
         validators_to_remove = updated_state_account.state.validators_to_remove;
         state_epoch = updated_state_account.state.current_epoch;
-        current_epoch = client.get_epoch_info().await?.epoch;
     }
 
-    Ok(())
+    Ok(stats)
 }
 
 async fn _handle_compute_score(
@@ -101,10 +113,60 @@ async fn _handle_compute_score(
     program_id: &Pubkey,
     all_steward_accounts: &Box<UsefulStewardAccounts>,
     priority_fee: Option<u64>,
-) -> Result<()> {
-    // println!("COMPUTE SCORE: {:?}", signature);
+) -> Result<SubmitStats, MonkeyCrankError> {
+    let validator_history_program_id = validator_history::id();
+    let cluster_history: Pubkey = get_cluster_history_address(&validator_history_program_id);
 
-    Ok(())
+    let validators_to_run = (0..all_steward_accounts.state_account.state.num_pool_validators)
+        .filter_map(|validator_index| {
+            let has_been_scored = all_steward_accounts
+                .state_account
+                .state
+                .progress
+                .get(validator_index)
+                .expect("Index is not in progress bitmask");
+            if has_been_scored {
+                None
+            } else {
+                let vote_account = all_steward_accounts.validator_list_account.validators
+                    [validator_index]
+                    .vote_account_address;
+                let history_account =
+                    get_validator_history_address(&vote_account, &validator_history_program_id);
+
+                Some((validator_index, vote_account, history_account))
+            }
+        })
+        .collect::<Vec<(usize, Pubkey, Pubkey)>>();
+
+    let ixs_to_run = validators_to_run
+        .iter()
+        .map(|(validator_index, _, history_account)| Instruction {
+            program_id: *program_id,
+            accounts: jito_steward::accounts::ComputeScore {
+                config: all_steward_accounts.config_address,
+                state_account: all_steward_accounts.state_address,
+                validator_history: *history_account,
+                validator_list: all_steward_accounts.validator_list_address,
+                cluster_history,
+                signer: payer.pubkey(),
+            }
+            .to_account_metas(None),
+            data: jito_steward::instruction::ComputeScore {
+                validator_list_index: *validator_index,
+            }
+            .data(),
+        })
+        .collect::<Vec<Instruction>>();
+
+    let txs_to_run = package_instructions(&ixs_to_run, 11, priority_fee, Some(1_400_000), None);
+
+    println!("Submitting {} instructions", ixs_to_run.len());
+    println!("Submitting {} transactions", txs_to_run.len());
+
+    let stats = submit_packaged_transactions(client, txs_to_run, &payer, None, None).await?;
+
+    Ok(stats)
 }
 
 async fn _handle_compute_delegations(
@@ -113,7 +175,7 @@ async fn _handle_compute_delegations(
     program_id: &Pubkey,
     all_steward_accounts: &Box<UsefulStewardAccounts>,
     priority_fee: Option<u64>,
-) -> Result<()> {
+) -> Result<SubmitStats, MonkeyCrankError> {
     let ix = Instruction {
         program_id: *program_id,
         accounts: jito_steward::accounts::ComputeDelegations {
@@ -125,24 +187,12 @@ async fn _handle_compute_delegations(
         data: jito_steward::instruction::ComputeDelegations {}.data(),
     };
 
-    let blockhash = client.get_latest_blockhash().await?;
-
     let configured_ix = configure_instruction(&[ix], priority_fee, None, None);
 
-    let transaction = Transaction::new_signed_with_payer(
-        &configured_ix,
-        Some(&payer.pubkey()),
-        &[&payer],
-        blockhash,
-    );
+    let stats =
+        submit_packaged_transactions(client, vec![configured_ix], &payer, None, None).await?;
 
-    let signature = client
-        .send_and_confirm_transaction_with_spinner(&transaction)
-        .await?;
-
-    println!("COMPUTE DELEGATION: {:?}", signature);
-
-    Ok(())
+    Ok(stats)
 }
 
 async fn _handle_idle(
@@ -151,10 +201,24 @@ async fn _handle_idle(
     program_id: &Pubkey,
     all_steward_accounts: &Box<UsefulStewardAccounts>,
     priority_fee: Option<u64>,
-) -> Result<()> {
-    // println!("IDLE: {:?}", signature);
+) -> Result<SubmitStats, MonkeyCrankError> {
+    let ix = Instruction {
+        program_id: *program_id,
+        accounts: jito_steward::accounts::Idle {
+            config: all_steward_accounts.config_address,
+            state_account: all_steward_accounts.state_address,
+            signer: payer.pubkey(),
+        }
+        .to_account_metas(None),
+        data: jito_steward::instruction::Idle {}.data(),
+    };
 
-    Ok(())
+    let configured_ix = configure_instruction(&[ix], priority_fee, None, None);
+
+    let stats =
+        submit_packaged_transactions(client, vec![configured_ix], &payer, None, None).await?;
+
+    Ok(stats)
 }
 
 async fn _handle_compute_instant_unstake(
@@ -163,10 +227,10 @@ async fn _handle_compute_instant_unstake(
     program_id: &Pubkey,
     all_steward_accounts: &Box<UsefulStewardAccounts>,
     priority_fee: Option<u64>,
-) -> Result<()> {
+) -> Result<SubmitStats, MonkeyCrankError> {
     // println!("IDLE: {:?}", signature);
 
-    Ok(())
+    Ok(SubmitStats::default())
 }
 
 async fn _handle_rebalance(
@@ -175,29 +239,29 @@ async fn _handle_rebalance(
     program_id: &Pubkey,
     all_steward_accounts: &Box<UsefulStewardAccounts>,
     priority_fee: Option<u64>,
-) -> Result<()> {
+) -> Result<SubmitStats, MonkeyCrankError> {
     // println!("IDLE: {:?}", signature);
 
-    Ok(())
+    Ok(SubmitStats::default())
 }
 
 pub async fn crank_monkey(
     client: &Arc<RpcClient>,
     payer: &Arc<Keypair>,
     program_id: &Pubkey,
+    epoch: u64,
     all_steward_accounts: &Box<UsefulStewardAccounts>,
     priority_fee: Option<u64>,
-) -> Result<()> {
-    // ----------- Triage -------------
+) -> Result<SubmitStats, MonkeyCrankError> {
     {
         // --------- CHECK AND HANDLE EPOCH BOUNDARY -----------
 
-        let epoch = client.get_epoch_info().await?.epoch;
         if all_steward_accounts.state_account.state.current_epoch != epoch {
             return _handle_epoch_maintenance(
                 &payer,
                 client,
                 &program_id,
+                epoch,
                 &all_steward_accounts,
                 priority_fee,
             )
@@ -269,7 +333,7 @@ pub async fn command_crank_monkey(
     args: CrankMonkey,
     client: &Arc<RpcClient>,
     program_id: Pubkey,
-) -> Result<()> {
+) -> Result<(), anyhow::Error> {
     // ----------- Collect Accounts -------------
     let steward_config = args.permissionless_parameters.steward_config;
     let payer = Arc::new(
@@ -285,12 +349,17 @@ pub async fn command_crank_monkey(
     let all_steward_accounts =
         get_all_steward_accounts(client, &program_id, &steward_config).await?;
 
-    return crank_monkey(
+    let epoch = client.get_epoch_info().await?.epoch;
+
+    crank_monkey(
         client,
         &payer,
         &program_id,
+        epoch,
         &all_steward_accounts,
         priority_fee,
     )
-    .await;
+    .await?;
+
+    Ok(())
 }
