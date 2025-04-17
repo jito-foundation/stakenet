2f0380ce3eb3948b7312fede51cd1ba2ec8e3613
Tommy Johnson <tommyjohnson1028@gmail.com>
2025-04-17 17:17:26 -0400

    add PriorityFeeCommissionEntry

--- keepers/stakenet-keeper/src/entries/priority_fee_commission_entry.rs
+++ keepers/stakenet-keeper/src/entries/priority_fee_commission_entry.rs
@@ -1,15 +1,30 @@
 use anchor_lang::{InstructionData, ToAccountMetas};
-use jito_tip_distribution_sdk::derive_tip_distribution_account_address;
+use jito_priority_fee_distribution::state::PriorityFeeDistributionAccount;
 use solana_program::{instruction::Instruction, pubkey::Pubkey};
 use stakenet_sdk::{
     models::entries::{Address, UpdateInstruction},
     utils::accounts::{get_validator_history_address, get_validator_history_config_address},
 };
 
+pub fn derive_priority_fee_distribution_account_address(
+  priority_fee_distribution_program_id: &Pubkey,
+  vote_pubkey: &Pubkey,
+  epoch: u64,
+) -> (Pubkey, u8) {
+  Pubkey::find_program_address(
+      &[
+          PriorityFeeDistributionAccount::SEED,
+          vote_pubkey.to_bytes().as_ref(),
+          epoch.to_le_bytes().as_ref(),
+      ],
+      priority_fee_distribution_program_id,
+  )
+}
+
 #[derive(Clone)]
-pub struct ValidatorMevCommissionEntry {
+pub struct PriorityFeeCommissionEntry {
     pub vote_account: Pubkey,
-    pub tip_distribution_account: Pubkey,
+    pub priority_fee_distribution_account: Pubkey,
     pub validator_history_account: Pubkey,
     pub config: Pubkey,
     pub program_id: Pubkey,
@@ -17,17 +32,17 @@ pub struct ValidatorMevCommissionEntry {
     pub epoch: u64,
 }
 
-impl ValidatorMevCommissionEntry {
+impl PriorityFeeCommissionEntry {
     pub fn new(
         vote_account: &Pubkey,
         epoch: u64,
         program_id: &Pubkey,
-        tip_distribution_program_id: &Pubkey,
+        priority_fee_distribution_program_id: &Pubkey,
         signer: &Pubkey,
     ) -> Self {
         let validator_history_account = get_validator_history_address(vote_account, program_id);
-        let (tip_distribution_account, _) = derive_tip_distribution_account_address(
-            tip_distribution_program_id,
+        let (priority_fee_distribution_account, _) = derive_priority_fee_distribution_account_address(
+          priority_fee_distribution_program_id,
             vote_account,
             epoch,
         );
@@ -35,7 +50,7 @@ impl ValidatorMevCommissionEntry {
 
         Self {
             vote_account: *vote_account,
-            tip_distribution_account,
+            priority_fee_distribution_account,
             validator_history_account,
             config,
             program_id: *program_id,
@@ -45,25 +60,25 @@ impl ValidatorMevCommissionEntry {
     }
 }
 
-impl Address for ValidatorMevCommissionEntry {
+impl Address for PriorityFeeCommissionEntry {
     fn address(&self) -> Pubkey {
         self.validator_history_account
     }
 }
 
-impl UpdateInstruction for ValidatorMevCommissionEntry {
+impl UpdateInstruction for PriorityFeeCommissionEntry {
     fn update_instruction(&self) -> Instruction {
         Instruction {
             program_id: self.program_id,
-            accounts: validator_history::accounts::CopyTipDistributionAccount {
+            accounts: validator_history::accounts::CopyPriorityFeeDistribution {
                 validator_history_account: self.validator_history_account,
                 vote_account: self.vote_account,
-                tip_distribution_account: self.tip_distribution_account,
                 config: self.config,
                 signer: self.signer,
+                distribution_account: self.priority_fee_distribution_account,
             }
             .to_account_metas(None),
-            data: validator_history::instruction::CopyTipDistributionAccount { epoch: self.epoch }
+            data: validator_history::instruction::CopyPriorityFeeDistribution { epoch: self.epoch }
                 .data(),
         }
     }
