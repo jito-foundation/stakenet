d14e48ffa1db8c0c75590f2ece4a33a610bc6190
Coach Chuck <christian@jito.wtf>
2025-06-02 09:10:31 -0600

    took out new error

--- programs/steward/src/errors.rs
+++ programs/steward/src/errors.rs
@@ -66,6 +66,4 @@ pub enum StewardError {
     ValidatorNeedsToBeMarkedForRemoval,
     #[msg("Invalid stake state")]
     InvalidStakeState,
-    #[msg("Arithmetic casting error")]
-    ArithmeticCastError,
 }
