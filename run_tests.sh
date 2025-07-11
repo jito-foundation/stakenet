970691b8e90248a186ceff75c798e3a932a4349d
Coach Chuck <christian@jito.wtf>
2025-07-11 15:35:57 -0600

    test

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,4 @@ cargo build-sbf --manifest-path programs/steward/Cargo.toml;
 cargo build-sbf --manifest-path programs/validator-history/Cargo.toml;
 
 # Run all tests
-SBF_OUT_DIR=$(pwd)/target/deploy RUST_MIN_STACK=10000000 cargo test --package tests --all-features --color auto
-
+SBF_OUT_DIR=$(pwd)/target/deploy RUST_MIN_STACK=10000000 cargo +1.84.1 test --package tests --all-features --color auto
