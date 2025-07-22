76df0855be7e9a76f812900191f5cfdc525ceae2
Alexander Binaei <bigtimetapin@gmail.com>
2025-07-22 14:50:39 -0700

    code tlc

--- programs/validator-history/src/instructions/update_stake_buffer.rs
+++ programs/validator-history/src/instructions/update_stake_buffer.rs
@@ -1,6 +1,5 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::epoch_stake::get_epoch_stake_for_vote_account;
-use solana_program::log::sol_log;
 
 use crate::{Config, ValidatorHistory, ValidatorStake, ValidatorStakeBuffer};
 
@@ -45,7 +44,7 @@ pub fn handle_update_stake_buffer(ctx: Context<UpdateStakeBuffer>) -> Result<()>
     // If the provided vote address corresponds to an account that is not a vote
     // account or does not exist, returns `0` for active stake.
     let stake_amount: u64 = get_epoch_stake_for_vote_account(&vote_account_pubkey);
-    sol_log(format!("{}: {}", vote_account_pubkey, stake_amount).as_str());
+
     // Insert into buffer
     let entry = ValidatorStake::new(validator_id, stake_amount);
     validator_stake_buffer.insert(config, entry)?;
