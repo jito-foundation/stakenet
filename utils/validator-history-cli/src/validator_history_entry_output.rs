64b155903b9fba899f2ff6572872c20834cb1421
aoikurokawa <ht.so.ik0102@gmail.com>
2025-08-07 04:22:02 +0900

    fix: use then_some

--- utils/validator-history-cli/src/validator_history_entry_output.rs
+++ utils/validator-history-cli/src/validator_history_entry_output.rs
@@ -68,134 +68,84 @@ impl From<ValidatorHistoryEntry> for ValidatorHistoryEntryOutput {
         let default_entry = ValidatorHistoryEntry::default();
 
         Self {
-            activated_stake_lamports: if value
+            activated_stake_lamports: (!value
                 .activated_stake_lamports
-                .eq(&default_entry.activated_stake_lamports)
-            {
-                None
-            } else {
-                Some(value.activated_stake_lamports.to_string())
-            },
-            mev_commission: if value.mev_commission.eq(&default_entry.mev_commission) {
-                None
-            } else {
-                Some(value.mev_commission.to_string())
-            },
-            epoch_credits: if value.epoch_credits.eq(&default_entry.epoch_credits) {
-                None
-            } else {
-                Some(value.epoch_credits.to_string())
-            },
-            commission: if value.commission.eq(&default_entry.commission) {
-                None
-            } else {
-                Some(value.commission.to_string())
-            },
-            client_type: if value.client_type.eq(&default_entry.client_type) {
-                None
-            } else {
-                Some(value.client_type.to_string())
-            },
-            version: if value.version.major.eq(&default_entry.version.major)
+                .eq(&default_entry.activated_stake_lamports))
+            .then_some(value.activated_stake_lamports.to_string()),
+
+            mev_commission: (!value.mev_commission.eq(&default_entry.mev_commission))
+                .then_some(value.mev_commission.to_string()),
+
+            epoch_credits: (!value.epoch_credits.eq(&default_entry.epoch_credits))
+                .then_some(value.epoch_credits.to_string()),
+
+            commission: (!value.commission.eq(&default_entry.commission))
+                .then_some(value.commission.to_string()),
+
+            client_type: (!value.client_type.eq(&default_entry.client_type))
+                .then_some(value.client_type.to_string()),
+
+            version: (!(value.version.major.eq(&default_entry.version.major)
                 && value.version.minor.eq(&default_entry.version.minor)
-                && value.version.patch.eq(&default_entry.version.patch)
-            {
-                None
-            } else {
-                Some(format!(
-                    "{}.{}.{}",
-                    value.version.major, value.version.minor, value.version.patch
-                ))
-            },
-            ip: if value.ip.eq(&default_entry.ip) {
-                None
-            } else {
-                Some(format!(
-                    "{}.{}.{}.{}",
-                    value.ip[0], value.ip[1], value.ip[2], value.ip[3]
-                ))
-            },
-            merkle_root_upload_authority: if value
+                && value.version.patch.eq(&default_entry.version.patch)))
+            .then_some(format!(
+                "{}.{}.{}",
+                value.version.major, value.version.minor, value.version.patch
+            )),
+
+            ip: (!value.ip.eq(&default_entry.ip)).then_some(format!(
+                "{}.{}.{}.{}",
+                value.ip[0], value.ip[1], value.ip[2], value.ip[3]
+            )),
+
+            merkle_root_upload_authority: (!value
                 .merkle_root_upload_authority
-                .eq(&default_entry.merkle_root_upload_authority)
-            {
-                None
-            } else {
-                Some((value.merkle_root_upload_authority as u8).to_string())
-            },
-            is_superminority: if value.is_superminority.eq(&default_entry.is_superminority) {
-                None
-            } else {
-                Some(value.is_superminority.to_string())
-            },
-            rank: if value.rank.eq(&default_entry.rank) {
-                None
-            } else {
-                Some(value.rank.to_string())
-            },
-            vote_account_last_update_slot: if value
+                .eq(&default_entry.merkle_root_upload_authority))
+            .then_some((value.merkle_root_upload_authority as u8).to_string()),
+
+            is_superminority: (!value.is_superminority.eq(&default_entry.is_superminority))
+                .then_some(value.is_superminority.to_string()),
+
+            rank: (!value.rank.eq(&default_entry.rank)).then_some(value.rank.to_string()),
+
+            vote_account_last_update_slot: (!value
                 .vote_account_last_update_slot
-                .eq(&default_entry.vote_account_last_update_slot)
-            {
-                None
-            } else {
-                Some(value.vote_account_last_update_slot.to_string())
-            },
-            mev_earned: if value.mev_earned.eq(&default_entry.mev_earned) {
-                None
-            } else {
-                Some((value.mev_earned as f64 / 100.0).to_string())
-            },
-            priority_fee_commission: if value
+                .eq(&default_entry.vote_account_last_update_slot))
+            .then_some(value.vote_account_last_update_slot.to_string()),
+
+            mev_earned: (!value.mev_earned.eq(&default_entry.mev_earned))
+                .then_some((value.mev_earned as f64 / 100.0).to_string()),
+
+            priority_fee_commission: (!value
                 .priority_fee_commission
-                .eq(&default_entry.priority_fee_commission)
-            {
-                None
-            } else {
-                Some(value.priority_fee_commission.to_string())
-            },
-            priority_fee_tips: if value.priority_fee_tips.eq(&default_entry.priority_fee_tips) {
-                None
-            } else {
-                Some(lamports_to_sol(value.priority_fee_tips).to_string())
-            },
-            total_priority_fees: if value
+                .eq(&default_entry.priority_fee_commission))
+            .then_some(value.priority_fee_commission.to_string()),
+
+            priority_fee_tips: (!value.priority_fee_tips.eq(&default_entry.priority_fee_tips))
+                .then_some(lamports_to_sol(value.priority_fee_tips).to_string()),
+
+            total_priority_fees: (!value
                 .total_priority_fees
-                .eq(&default_entry.total_priority_fees)
-            {
-                None
-            } else {
-                Some(lamports_to_sol(value.total_priority_fees).to_string())
-            },
-            total_leader_slots: if value
+                .eq(&default_entry.total_priority_fees))
+            .then_some(lamports_to_sol(value.total_priority_fees).to_string()),
+
+            total_leader_slots: (!value
                 .total_leader_slots
-                .eq(&default_entry.total_leader_slots)
-            {
-                None
-            } else {
-                Some(value.total_leader_slots.to_string())
-            },
-            blocks_produced: if value.blocks_produced.eq(&default_entry.blocks_produced) {
-                None
-            } else {
-                Some(value.blocks_produced.to_string())
-            },
-            block_data_updated_at_slot: if value
+                .eq(&default_entry.total_leader_slots))
+            .then_some(value.total_leader_slots.to_string()),
+
+            blocks_produced: (!value.blocks_produced.eq(&default_entry.blocks_produced))
+                .then_some(value.blocks_produced.to_string()),
+
+            block_data_updated_at_slot: (!value
                 .block_data_updated_at_slot
-                .eq(&default_entry.block_data_updated_at_slot)
-            {
-                None
-            } else {
-                Some(value.block_data_updated_at_slot.to_string())
-            },
-            priority_fee_merkle_root_upload_authority: if value
+                .eq(&default_entry.block_data_updated_at_slot))
+            .then_some(value.block_data_updated_at_slot.to_string()),
+
+            priority_fee_merkle_root_upload_authority: (!value
                 .priority_fee_merkle_root_upload_authority
-                .eq(&default_entry.priority_fee_merkle_root_upload_authority)
-            {
-                None
-            } else {
-                Some((value.priority_fee_merkle_root_upload_authority as u8).to_string())
-            },
+                .eq(&default_entry.priority_fee_merkle_root_upload_authority))
+            .then_some((value.priority_fee_merkle_root_upload_authority as u8).to_string()),
         }
     }
 }
