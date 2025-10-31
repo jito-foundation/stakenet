2e7026cec0b2775e93b2515ca74faab98c644c11
aoikurokawa <ht.so.ik0102@gmail.com>
2025-11-01 07:38:40 +0900

    fix: program id

--- programs/steward/src/lib.rs
+++ programs/steward/src/lib.rs
@@ -22,7 +22,7 @@ pub mod utils;
 
 pub use state::*;
 
-declare_id!("3YeBnUPN2ZW8MBVb8695Hdffu8jBpRjm6BUazRexHDTg");
+declare_id!("Stewardf95sJbmtcZsyagb2dg4Mo8eVQho8gpECvLx8");
 
 /*
 This program manages the selection of validators and delegation of stake for a SPL Stake Pool.
