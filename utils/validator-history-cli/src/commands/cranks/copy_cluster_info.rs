9e0c7ecb3329ccbe1f38fbab43aaff4feddb6a4e
aoikurokawa <ht.so.ik0102@gmail.com>
2025-11-29 10:02:36 +0900

    fix: compute budget

--- utils/validator-history-cli/src/commands/cranks/copy_cluster_info.rs
+++ utils/validator-history-cli/src/commands/cranks/copy_cluster_info.rs
@@ -1,15 +1,15 @@
 use std::{path::PathBuf, sync::Arc};
 
-use anchor_lang::{AccountDeserialize, InstructionData, ToAccountMetas};
+use anchor_lang::{InstructionData, ToAccountMetas};
 use anyhow::anyhow;
 use clap::Parser;
 use solana_client::nonblocking::rpc_client::RpcClient;
+#[allow(deprecated)]
 use solana_sdk::{
-    instruction::Instruction, signature::read_keypair_file, signer::Signer,
+    compute_budget, instruction::Instruction, signature::read_keypair_file, signer::Signer,
     transaction::Transaction,
 };
 use stakenet_sdk::utils::accounts::get_cluster_history_address;
-use validator_history::ClusterHistory;
 
 #[derive(Parser)]
 #[command(about = "Crank to copy cluster info data to cluster history accounts")]
@@ -26,24 +26,11 @@ pub async fn run(args: CrankCopyClusterInfo, rpc_url: String) -> anyhow::Result<
     let client = RpcClient::new(rpc_url);
     let client = Arc::new(client);
 
-    let epoch_info = client.get_epoch_info().await?;
-    let epoch = epoch_info.epoch;
-
     let cluster_history_address = get_cluster_history_address(&validator_history::id());
-    let cluster_history_acc_data = client.get_account_data(&cluster_history_address).await?;
-    let cluster_history =
-        ClusterHistory::try_deserialize(&mut cluster_history_acc_data.as_slice())?;
-
-    if cluster_history
-        .history
-        .arr
-        .iter()
-        .any(|entry| entry.epoch.eq(&(epoch as u16)))
-    {
-        println!("Cluster History has already updated");
-        return Ok(());
-    }
-
+    let priority_fee_ix = compute_budget::ComputeBudgetInstruction::set_compute_unit_price(20000);
+    let heap_request_ix = compute_budget::ComputeBudgetInstruction::request_heap_frame(256 * 1024);
+    let compute_budget_ix =
+        compute_budget::ComputeBudgetInstruction::set_compute_unit_limit(1_400_000);
     let update_instruction = Instruction {
         program_id: validator_history::id(),
         accounts: validator_history::accounts::CopyClusterInfo {
@@ -60,7 +47,12 @@ pub async fn run(args: CrankCopyClusterInfo, rpc_url: String) -> anyhow::Result<
         .await
         .expect("Failed to fetch latest blockhash");
     let transaction = Transaction::new_signed_with_payer(
-        &[update_instruction],
+        &[
+            priority_fee_ix,
+            heap_request_ix,
+            compute_budget_ix,
+            update_instruction,
+        ],
         Some(&keypair.pubkey()),
         &[keypair],
         hash,
