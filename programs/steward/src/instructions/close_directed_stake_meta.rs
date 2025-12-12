34c48cf4121902b261a45dfb4a48ec13d66da30a
gzalz <eric.gonzalez@jito.wtf>
2025-12-12 13:11:59 -0700

    steward: close meta ixn, conditional progress output

---
+++ programs/steward/src/instructions/close_directed_stake_meta.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+
+use crate::{errors::StewardError, Config, DirectedStakeMeta};
+use std::mem::size_of;
+
+#[derive(Accounts)]
+pub struct CloseDirectedStakeMeta<'info> {
+    #[account()]
+    pub config: AccountLoader<'info, Config>,
+
+    #[account(
+        mut,
+        close = authority,
+        seeds = [DirectedStakeMeta::SEED, config.key().as_ref()],
+        bump
+    )]
+    pub directed_stake_meta: AccountLoader<'info, DirectedStakeMeta>,
+
+    #[account(
+        mut,
+        address = config.load()?.admin @ StewardError::Unauthorized
+    )]
+    pub authority: Signer<'info>,
+}
+
+impl CloseDirectedStakeMeta<'_> {
+    pub const SIZE: usize = 8 + size_of::<Self>();
+}
+
+pub fn handler(_ctx: Context<CloseDirectedStakeMeta>) -> Result<()> {
+    Ok(())
+}
