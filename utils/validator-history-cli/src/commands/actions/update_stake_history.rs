18b884d9593296eeb91566872b2f7deed566c8f8
aoikurokawa <ht.so.ik0102@gmail.com>
2025-12-17 11:06:20 +0900

    feat: update stake history

---
+++ utils/validator-history-cli/src/commands/actions/update_stake_history.rs
@@ -0,0 +1,75 @@
+use std::{path::PathBuf, sync::Arc};
+
+use anyhow::anyhow;
+use clap::Parser;
+use solana_client::{nonblocking::rpc_client::RpcClient, rpc_response::RpcVoteAccountInfo};
+use solana_sdk::{
+    pubkey::Pubkey, signature::read_keypair_file, signer::Signer, transaction::Transaction,
+};
+use stakenet_keeper::{
+    entries::stake_history_entry::StakeHistoryEntry,
+    operations::stake_upload::get_stake_rank_map_and_superminority_count,
+};
+use stakenet_sdk::models::entries::UpdateInstruction;
+
+#[derive(Parser)]
+#[command(about = "Crank to copy vote account data to validator history accounts")]
+pub struct UpdateStakeHistory {
+    /// Path to keypair for transaction signing
+    #[arg(short, long, env, default_value = "~/.config/solana/id.json")]
+    keypair_path: PathBuf,
+
+    /// Only process validators returned by get_vote_accounts RPC (active validators)
+    #[arg(long, env)]
+    vote_account: Pubkey,
+}
+
+pub async fn run(args: UpdateStakeHistory, rpc_url: String) -> anyhow::Result<()> {
+    let keypair = read_keypair_file(args.keypair_path)
+        .map_err(|e| anyhow!("Failed reading keypair file: {e}"))?;
+    let keypair = Arc::new(keypair);
+    let client = RpcClient::new(rpc_url);
+    let client = Arc::new(client);
+
+    let vote_accounts = client.get_vote_accounts().await?;
+    let vote_accounts: Vec<&RpcVoteAccountInfo> = vote_accounts.current.iter().map(|v| v).collect();
+    let epoch_info = client.get_epoch_info().await?;
+
+    let (stake_rank_map, superminority_threshold) =
+        get_stake_rank_map_and_superminority_count(&vote_accounts);
+
+    let rank = stake_rank_map[&args.vote_account.to_string()];
+    let is_superminority = rank <= superminority_threshold;
+
+    for vote_account_info in vote_accounts {
+        if vote_account_info.vote_pubkey == args.vote_account.to_string() {
+            let copy_vote_account_entry = StakeHistoryEntry::new(
+                vote_account_info,
+                &validator_history::id(),
+                &keypair.pubkey(),
+                epoch_info.epoch,
+                rank,
+                is_superminority,
+            );
+
+            let update_instruction = copy_vote_account_entry.update_instruction();
+
+            let hash = client
+                .get_latest_blockhash()
+                .await
+                .map_err(|e| anyhow!("Failed to fetch latest blockhash: {e}"))?;
+            let transaction = Transaction::new_signed_with_payer(
+                &[update_instruction],
+                Some(&keypair.pubkey()),
+                &[keypair.clone()],
+                hash,
+            );
+            let signature = client.send_transaction(&transaction).await?;
+
+            println!("Submit Result: {signature:?}");
+            break;
+        }
+    }
+
+    Ok(())
+}
--- utils/validator-history-cli/src/main.rs
+++ utils/validator-history-cli/src/main.rs
@@ -22,6 +22,7 @@ use validator_history::{
 use validator_history_cli::{
     commands::{
         self,
+        actions::update_stake_history::UpdateStakeHistory,
         cranks::{
             copy_cluster_info::CrankCopyClusterInfo,
             copy_gossip_contact_info::CrankCopyGossipContactInfo,
@@ -63,6 +64,9 @@ enum Commands {
     UpdateOracleAuthority(UpdateOracleAuthority),
     DunePriorityFeeBackfill(DunePriorityFeeBackfill),
     UploadValidatorAge(UploadValidatorAge),
+
+    // Actions
+    UpdateStakeHistory(UpdateStakeHistory),
 
     // Cranks
     CrankCopyClusterInfo(CrankCopyClusterInfo),
@@ -1287,6 +1291,9 @@ async fn main() -> anyhow::Result<()> {
         Commands::UploadValidatorAge(args) => command_upload_validator_age(args, client),
         Commands::BackfillValidatorAge(command_args) => {
             commands::backfill_validator_age::run(command_args, args.json_rpc_url).await
+        }
+        Commands::UpdateStakeHistory(command_args) => {
+            commands::actions::update_stake_history::run(command_args, args.json_rpc_url).await?
         }
         Commands::CrankCopyClusterInfo(command_args) => {
             commands::cranks::copy_cluster_info::run(command_args, args.json_rpc_url).await?
