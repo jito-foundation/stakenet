23c74d08bc125e7873e48c942eb90b65eb35059e
aoikurokawa <ht.so.ik0102@gmail.com>
2026-02-11 04:59:16 +0900

    fix: tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,6 +5,6 @@ cargo build-sbf --manifest-path programs/steward/Cargo.toml;
 cargo build-sbf --manifest-path programs/validator-history/Cargo.toml;
 
 # Run all tests
-SBF_OUT_DIR=$(pwd)/target/sbpf-solana-solana/release/ RUST_MIN_STACK=20000000 cargo nextest run -p tests --all-features --color auto
-SBF_OUT_DIR=$(pwd)/target/sbpf-solana-solana/release/ RUST_MIN_STACK=20000000 cargo nextest run -p stakenet-sdk  --all-features --color auto
-SBF_OUT_DIR=$(pwd)/target/sbpf-solana-solana/release/ RUST_MIN_STACK=20000000 cargo nextest run -p jito-steward --all-features --color auto
+SBF_OUT_DIR=$(pwd)/target/deploy RUST_MIN_STACK=20000000 cargo nextest run -p tests --all-features --color auto
+SBF_OUT_DIR=$(pwd)/target/deploy RUST_MIN_STACK=20000000 cargo nextest run -p stakenet-sdk  --all-features --color auto
+SBF_OUT_DIR=$(pwd)/target/deploy RUST_MIN_STACK=20000000 cargo nextest run -p jito-steward --all-features --color auto
