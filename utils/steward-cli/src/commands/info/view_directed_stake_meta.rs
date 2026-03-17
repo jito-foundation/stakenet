4367ee03c32cc48f2b0b194541acf8b3e6b40726
aoikurokawa <ht.so.ik0102@gmail.com>
2026-03-17 16:33:01 -0600

    fix: audit and clippy

--- utils/steward-cli/src/commands/info/view_directed_stake_meta.rs
+++ utils/steward-cli/src/commands/info/view_directed_stake_meta.rs
@@ -33,22 +33,20 @@ pub async fn command_view_directed_stake_meta(
     println!("\n🎯 Stake Targets:");
     for i in 0..stake_meta.total_stake_targets as usize {
         let validator = &stake_meta.targets[i];
-        if validator.staked_last_updated_epoch != 942 {
-            if validator.vote_pubkey != Pubkey::default() {
-                println!("  Target {}:", i + 1);
-                println!("    Vote Pubkey: {}", validator.vote_pubkey);
-                println!("    Target Lamports: {}", validator.total_target_lamports);
-                println!(
-                    "    Target Last Updated Epoch: {}",
-                    validator.target_last_updated_epoch
-                );
-                println!("    Staked Lamports: {}", validator.total_staked_lamports);
-                println!(
-                    "    Staked Last Updated Epoch: {}",
-                    validator.staked_last_updated_epoch
-                );
-                println!();
-            }
+        if validator.vote_pubkey != Pubkey::default() {
+            println!("  Target {}:", i + 1);
+            println!("    Vote Pubkey: {}", validator.vote_pubkey);
+            println!("    Target Lamports: {}", validator.total_target_lamports);
+            println!(
+                "    Target Last Updated Epoch: {}",
+                validator.target_last_updated_epoch
+            );
+            println!("    Staked Lamports: {}", validator.total_staked_lamports);
+            println!(
+                "    Staked Last Updated Epoch: {}",
+                validator.staked_last_updated_epoch
+            );
+            println!();
         }
     }
 
