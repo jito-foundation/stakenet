546df97c7c21dad07ed8d23522061ed65976df37
Aoi K <62386689+aoikurokawa@users.noreply.github.com>
2026-03-24 03:02:58 +0900

    CLI: Crank Instant Remove validators (#237)
    ```bash
     ./target/release/steward-cli --json-rpc-url  crank-instant-remove-validators --payer-keypair-path ~/.config/solana/id.json --steward-config 5pZmpk3ktweGZW9xFknpEHhQoWeAKTzSGwnCUyVdiye
    
    ```

--- utils/steward-cli/src/commands/command_args.rs
+++ utils/steward-cli/src/commands/command_args.rs
@@ -36,6 +36,7 @@ use crate::commands::{
     },
     cranks::{
         compute_directed_stake_meta::ComputeDirectedStakeMeta,
+        instant_remove_validators::CrankInstantRemoveValidators,
         rebalance_directed::CrankRebalanceDirected,
     },
     info::{view_blacklist::ViewBlacklist, view_directed_stake_ticket::ViewDirectedStakeTicket},
@@ -390,6 +391,7 @@ pub enum Commands {
     CrankRebalance(CrankRebalance),
     CrankRebalanceDirected(CrankRebalanceDirected),
     CrankUpdateStakePool(CrankUpdateStakePool),
+    CrankInstantRemoveValidators(CrankInstantRemoveValidators),
 }
 
 // ---------- VIEWS ------------
---
+++ utils/steward-cli/src/commands/cranks/instant_remove_validators.rs
@@ -0,0 +1,106 @@
+use std::sync::Arc;
+
+use anchor_lang::{InstructionData, ToAccountMetas};
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_program::instruction::Instruction;
+use solana_sdk::{pubkey::Pubkey, signature::read_keypair_file};
+use stakenet_sdk::{
+    models::submit_stats::SubmitStats,
+    utils::{
+        accounts::{get_all_steward_accounts, get_directed_stake_meta_address},
+        transactions::configure_instruction,
+    },
+};
+
+use crate::{
+    commands::command_args::PermissionlessParameters,
+    utils::transactions::submit_packaged_transactions,
+};
+
+#[derive(Parser)]
+#[command(about = "Crank instant remove validators")]
+pub struct CrankInstantRemoveValidators {
+    #[command(flatten)]
+    pub permissionless_parameters: PermissionlessParameters,
+}
+
+pub async fn command_crank_instant_remove_validators(
+    args: CrankInstantRemoveValidators,
+    client: &Arc<RpcClient>,
+    program_id: Pubkey,
+) -> Result<()> {
+    let args = args.permissionless_parameters;
+
+    // Creates config account
+    let payer = read_keypair_file(args.payer_keypair_path)
+        .map_err(|e| anyhow!("Failed reading keypair file ( Payer ): {e}"))?;
+    let payer = Arc::new(payer);
+
+    let steward_config = args.steward_config;
+
+    let all_steward_accounts =
+        get_all_steward_accounts(client, &program_id, &steward_config).await?;
+
+    let num_validators = all_steward_accounts.state_account.state.num_pool_validators;
+    let mut validators_to_remove = all_steward_accounts
+        .state_account
+        .state
+        .validators_for_immediate_removal;
+
+    let mut stats = SubmitStats::default();
+
+    while validators_to_remove.count() != 0 {
+        let all_steward_accounts =
+            get_all_steward_accounts(client, &program_id, &steward_config).await?;
+
+        let mut validator_index_to_remove = None;
+        for i in 0..all_steward_accounts.validator_list_account.validators.len() as u64 {
+            if validators_to_remove.get(i as usize).map_err(|e| {
+                anyhow!(
+                    "Error fetching bitmask index for immediate removed validator: {i}/{num_validators} - {e}",
+                )
+            })? {
+                validator_index_to_remove = Some(i);
+                break;
+            }
+        }
+
+        let directed_stake_meta =
+            get_directed_stake_meta_address(&all_steward_accounts.config_address, &program_id);
+
+        let ix = Instruction {
+            program_id,
+            accounts: jito_steward::accounts::InstantRemoveValidator {
+                config: all_steward_accounts.config_address,
+                state_account: all_steward_accounts.state_address,
+                validator_list: all_steward_accounts.validator_list_address,
+                stake_pool: all_steward_accounts.stake_pool_address,
+                directed_stake_meta,
+            }
+            .to_account_metas(None),
+            data: jito_steward::instruction::InstantRemoveValidator {
+                validator_index_to_remove: validator_index_to_remove.unwrap(),
+            }
+            .data(),
+        };
+
+        let configured_ix = configure_instruction(&[ix], None, Some(1_400_000), None);
+
+        log::info!("Submitting Instant Removal");
+        let new_stats =
+            submit_packaged_transactions(client, vec![configured_ix], &payer, Some(50), None)
+                .await?;
+
+        let all_steward_accounts =
+            get_all_steward_accounts(client, &program_id, &steward_config).await?;
+        validators_to_remove = all_steward_accounts
+            .state_account
+            .state
+            .validators_for_immediate_removal;
+        stats.combine(&new_stats);
+    }
+
+    Ok(())
+}
