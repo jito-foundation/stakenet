0dfc1ed9eefae90da306991a7d2375808fc1b74c
aoikurokawa <ht.so.ik0102@gmail.com>
2026-06-04 05:09:56 +0900

    fix: disable ticketing system directed delegation now

--- keepers/stakenet-keeper/src/entries/crank_copy_directed_stake_targets.rs
+++ keepers/stakenet-keeper/src/entries/crank_copy_directed_stake_targets.rs
@@ -15,6 +15,9 @@ use stakenet_sdk::{
 use crate::state::keeper_config::KeeperConfig;
 
 /// Copy directed stake targets to [`DirectedStakeMeta`] account
+///
+/// NOTE: The normal (ticketing system) directed-stake path via `compute_directed_stake_meta` is not
+/// used here.
 pub(crate) async fn crank_copy_directed_stake_targets(
     keeper_config: &KeeperConfig,
     keypair: Arc<Keypair>,
@@ -31,24 +34,24 @@ pub(crate) async fn crank_copy_directed_stake_targets(
     } = keeper_config;
     let mut stats = SubmitStats::default();
 
-    let normal_ixs = compute_directed_stake_meta(
-        client.clone(),
-        token_mint_address,
-        &all_steward_accounts.stake_pool_address,
-        &all_steward_accounts.config_address,
-        &keypair.pubkey(),
-        program_id,
-    )
-    .await
-    .map_err(|e| JitoTransactionError::Custom(e.to_string()))?;
+    // let normal_ixs = compute_directed_stake_meta(
+    //     client.clone(),
+    //     token_mint_address,
+    //     &all_steward_accounts.stake_pool_address,
+    //     &all_steward_accounts.config_address,
+    //     &keypair.pubkey(),
+    //     program_id,
+    // )
+    // .await
+    // .map_err(|e| JitoTransactionError::Custom(e.to_string()))?;
 
-    log::info!("Normal copy directed stake targets: {}", normal_ixs.len());
+    // log::info!("Normal copy directed stake targets: {}", normal_ixs.len());
 
-    let normal_txs_to_run =
-        package_instructions(&normal_ixs, 8, Some(*priority_fee), Some(1_400_000), None);
-    let normal_stats =
-        submit_packaged_transactions(client, normal_txs_to_run, &keypair, Some(50), None).await?;
-    stats.combine(&normal_stats);
+    // let normal_txs_to_run =
+    //     package_instructions(&normal_ixs, 8, Some(*priority_fee), Some(1_400_000), None);
+    // let normal_stats =
+    //     submit_packaged_transactions(client, normal_txs_to_run, &keypair, Some(50), None).await?;
+    // stats.combine(&normal_stats);
 
     let coinbase_delegation_ixs = compute_coinbase_targets(
         client.clone(),
