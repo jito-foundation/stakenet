4e84c22503b505587b5ebe0a00e0630253d85095
aoikurokawa <ht.so.ik0102@gmail.com>
2026-06-23 08:44:18 +0900

    fix: uncomment normal ixs

--- keepers/stakenet-keeper/src/entries/crank_copy_directed_stake_targets.rs
+++ keepers/stakenet-keeper/src/entries/crank_copy_directed_stake_targets.rs
@@ -7,7 +7,7 @@ use stakenet_sdk::{
         submit_stats::SubmitStats,
     },
     utils::{
-        instructions::compute_coinbase_targets,
+        instructions::{compute_coinbase_targets, compute_directed_stake_meta},
         transactions::{package_instructions, submit_packaged_transactions},
     },
 };
@@ -23,7 +23,7 @@ pub async fn crank_copy_directed_stake_targets(
     let KeeperConfig {
         client,
         steward_program_id: program_id,
-        token_mint: _token_mint_address,
+        token_mint,
         priority_fee_in_microlamports: priority_fee,
         kobe_client,
         coinbase_vote_pubkey,
@@ -31,24 +31,24 @@ pub async fn crank_copy_directed_stake_targets(
     } = keeper_config;
     let mut stats = SubmitStats::default();
 
-    // let normal_ixs = compute_directed_stake_meta(
-    //     client.clone(),
-    //     token_mint_address,
-    //     &all_steward_accounts.stake_pool_address,
-    //     &all_steward_accounts.config_address,
-    //     &keypair.pubkey(),
-    //     program_id,
-    // )
-    // .await
-    // .map_err(|e| JitoTransactionError::Custom(e.to_string()))?;
+    let normal_ixs = compute_directed_stake_meta(
+        client.clone(),
+        token_mint,
+        &all_steward_accounts.stake_pool_address,
+        &all_steward_accounts.config_address,
+        &keypair.pubkey(),
+        program_id,
+    )
+    .await
+    .map_err(|e| JitoTransactionError::Custom(e.to_string()))?;
 
-    // log::info!("Normal copy directed stake targets: {}", normal_ixs.len());
+    log::info!("Normal copy directed stake targets: {}", normal_ixs.len());
 
-    // let normal_txs_to_run =
-    //     package_instructions(&normal_ixs, 8, Some(*priority_fee), Some(1_400_000), None);
-    // let normal_stats =
-    //     submit_packaged_transactions(client, normal_txs_to_run, &keypair, Some(50), None).await?;
-    // stats.combine(&normal_stats);
+    let normal_txs_to_run =
+        package_instructions(&normal_ixs, 8, Some(*priority_fee), Some(1_400_000), None);
+    let normal_stats =
+        submit_packaged_transactions(client, normal_txs_to_run, &keypair, Some(50), None).await?;
+    stats.combine(&normal_stats);
 
     let coinbase_delegation_ixs = compute_coinbase_targets(
         client.clone(),
