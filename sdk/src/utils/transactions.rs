f341051e9ae5ad55cc9e6d0591fa7638b29a52da
aoikurokawa <ht.so.ik0102@gmail.com>
2026-07-18 08:11:57 +0900

    fix: clippy

--- sdk/src/utils/transactions.rs
+++ sdk/src/utils/transactions.rs
@@ -183,7 +183,7 @@ pub async fn get_vote_accounts_with_retry(
             return Ok(response
                 .current
                 .into_iter()
-                .chain(response.delinquent.into_iter())
+                .chain(response.delinquent)
                 .filter(|vote_account| vote_account.epoch_credits.len() >= min_vote_epochs)
                 .collect::<Vec<_>>());
         }
@@ -195,7 +195,7 @@ pub async fn get_vote_accounts_with_retry(
         Ok(response) => Ok(response
             .current
             .into_iter()
-            .chain(response.delinquent.into_iter())
+            .chain(response.delinquent)
             .filter(|vote_account| vote_account.epoch_credits.len() >= min_vote_epochs)
             .collect::<Vec<_>>()),
         Err(e) => Err(e),
